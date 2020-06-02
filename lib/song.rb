5d488b04c541b5c8d8c0c625ec6be4f8a5759451
Kamille Longalong <k.pagibigan@gmail.com>
2020-06-02 03:47:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,8 +20,9 @@ class Song
     new_song
   end
   
-  def artist_name=(artist_name)
-    #self.artist.nil? ? nil : 
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
   end
   
 end
